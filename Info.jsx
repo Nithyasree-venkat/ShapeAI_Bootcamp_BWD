dd5e2e0262d9bbdcf7ad73d97580408faef1e479
Nithyasree-venkat <85869179+Nithyasree-venkat@users.noreply.github.com>
2021-06-27 19:00:31 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>A basic wed dev React js Bootcamp.Final Project Submission.</p>
+    </div>
+  );
+}
+
+export default Info;
